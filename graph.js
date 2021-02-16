a2c65a503f1748cb5ae778f129b85d2f1ddefbb2
alexiliev <iliev.alex@outlook.com>
2021-02-16 11:22:15 -0600

    adding event listener

--- graph.js
+++ graph.js
@@ -23,3 +23,39 @@ const pie = d3.pie()
                 .sort(null) //don't re-sort my data based on size
                 .value(d => d.cost) //generate angles based on cost from firestore
 
+//arc path generator which makes the slice shapes. needs start and end angles
+const arcPath = d3.arc()
+                    .outerRadius(dims.radius)
+                    .innerRadius(dims.radius / 2)
+
+
+//update function
+const update = (data) => {
+    console.log(data)
+}
+//firestore listener
+let data = []
+db.collection('expenses').onSnapshot(res => {
+    res.docChanges().forEach(change => {
+        const doc = {...change.doc.data(), id: change.doc.id}
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc)
+                break;
+            case 'modified':
+                //cahnge doc in array if the id of the snapshot matches
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                //if true item remain in array, otherwise it's removed
+                data = data.filter(item => item.id != doc.id)
+                break;
+            default:
+                break;
+        }
+    })
+});
+
+update(data)
