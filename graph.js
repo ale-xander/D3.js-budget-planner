9fafbe28715433dd87167a0738db8d385af93a7c
alexiliev <iliev.alex@outlook.com>
2021-02-16 12:43:26 -0600

    update function for pie and arc

--- graph.js
+++ graph.js
@@ -1,38 +1,48 @@
 console.log('graph.js connected')
 
-const dims = {
-    height:300,
-    width: 300,
-    radius: 150
-}
-const center = {
-    x: (dims.width / 2 + 5),
-    y: (dims.height / 2 + 5)
-}
+const dims = { height: 300, width: 300, radius: 150 };
+const center = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5)};
 
+// create svg container
 const svg = d3.select('.canvas')
-            .append('svg')
-            .attr('width', dims.width + 150)//leaving 150px for legend on the right
-            .attr('height', dims.height + 150)
+  .append('svg')
+  .attr('width', dims.width + 150)
+  .attr('height', dims.height + 150);
 
-const graph = svg.append('g') //append group
-                .attr('transform', `translate(${center.x}, ${center.y})`)
+// translates the graph group to the middle of the svg container
+const graph = svg.append('g')
+  .attr("transform", `translate(${center.x}, ${center.y})`);
 
 //retrun a func that generates angles. you get start and end angle for each
 const pie = d3.pie()
                 .sort(null) //don't re-sort my data based on size
                 .value(d => d.cost) //generate angles based on cost from firestore
+                
 
 //arc path generator which makes the slice shapes. needs start and end angles
 const arcPath = d3.arc()
-                    .outerRadius(dims.radius)
-                    .innerRadius(dims.radius / 2)
+  .outerRadius(dims.radius)
+  .innerRadius(dims.radius / 2);
 
 
-//update function
+//update function to pass thru pie and arc generator
 const update = (data) => {
-    console.log(data)
-}
+  
+    // join enhanced (pie) data to path elements
+    const paths = graph.selectAll('path')
+      .data(pie(data));//get data with angles attached
+  
+    console.log(paths.enter());
+  
+    paths.enter()
+      .append('path')
+        .attr('class', 'arc')
+        .attr('d', arcPath)
+        .attr('stroke', '#fff')
+        .attr('stroke-width', 3);
+  console.log('lol')
+  };
+
 //firestore listener
 let data = []
 db.collection('expenses').onSnapshot(res => {
@@ -56,6 +66,6 @@ db.collection('expenses').onSnapshot(res => {
                 break;
         }
     })
+    update(data)
 });
 
-update(data)
